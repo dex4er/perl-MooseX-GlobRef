b6e1d6e68baee1e7abda51c85d7eb3e90fbd5976
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2010-09-22 17:20:39 +0200

    Add URL to github. Bump version and year.

--- lib/MooseX/GlobRef.pm
+++ lib/MooseX/GlobRef.pm
@@ -60,7 +60,7 @@ use 5.006;
 use strict;
 use warnings;
 
-our $VERSION = '0.07';
+our $VERSION = '0.0701';
 
 
 use Moose 0.56 ();
@@ -112,7 +112,7 @@ sub init_meta {
 
 =head1 SEE ALSO
 
-L<Moose>.
+L<http://github.com/dex4er/perl-MooseX-GlobRef>, L<Moose>.
 
 =for readme continue
 
@@ -122,9 +122,9 @@ Piotr Roszatycki <dexter@cpan.org>
 
 =head1 LICENSE
 
-Copyright (C) 2007, 2008, 2009 by Piotr Roszatycki E<lt>dexter@cpan.orgE<gt>.
+Copyright (c) 2007, 2008, 2009, 2010 Piotr Roszatycki <dexter@cpan.org>.
 
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
+This is free software; you can redistribute it and/or modify it under
+the same terms as perl itself.
 
-See L<http://www.perl.com/perl/misc/Artistic.html>
+See L<http://dev.perl.org/licenses/artistic.html>
